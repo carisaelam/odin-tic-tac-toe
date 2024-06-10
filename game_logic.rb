761b8d27166186e78b81d2d387190deb8f870183
carisaelam <carisaelam@gmail.com>
2024-06-10 08:57:09 -0400

    Move all game logic into GameLogic module
    Separated all of the logic into its own module
    and reserved the TicTacToe class for running
    one method called 'start' that handles the
    flow of the game using methods from GameLogic
    module.

--- game_logic.rb
+++ game_logic.rb
@@ -1,6 +1,5 @@
 require 'colorize'
 require 'colorized_string'
-require 'pry-byebug'
 
 module GameLogic
   def self.winning_combinations
@@ -11,6 +10,9 @@ module GameLogic
     @row1 = %w[1 2 3]
     @row2 = %w[4 5 6]
     @row3 = %w[7 8 9]
+    @game_over = false
+    @p1_name = 'Player One'
+    @p2_name = 'Player Two'
     @p1_positions = []
     @p2_positions = []
     @winning_combos = [
@@ -25,24 +27,6 @@ module GameLogic
     ]
     @winner = nil
     @positions_taken = []
-  end
-
-  def mark_o(position)
-    case position
-    when 1 then @row1[0] = 'O'
-    when 2 then @row1[1] = 'O'
-    when 3 then @row1[2] = 'O'
-    when 4 then @row2[0] = 'O'
-    when 5 then @row2[1] = 'O'
-    when 6 then @row2[2] = 'O'
-    when 7 then @row3[0] = 'O'
-    when 8 then @row3[1] = 'O'
-    when 9 then @row3[2] = 'O'
-    else
-      puts 'select a valid position'
-    end
-    @p2_positions.push(position)
-    @positions_taken.push(position)
   end
 
   def mark_x(position)
@@ -63,6 +47,24 @@ module GameLogic
     @positions_taken.push(position)
   end
 
+  def mark_o(position)
+    case position
+    when 1 then @row1[0] = 'O'
+    when 2 then @row1[1] = 'O'
+    when 3 then @row1[2] = 'O'
+    when 4 then @row2[0] = 'O'
+    when 5 then @row2[1] = 'O'
+    when 6 then @row2[2] = 'O'
+    when 7 then @row3[0] = 'O'
+    when 8 then @row3[1] = 'O'
+    when 9 then @row3[2] = 'O'
+    else
+      puts 'select a valid position'
+    end
+    @p2_positions.push(position)
+    @positions_taken.push(position)
+  end
+
   def print_board
     p @row1.join(' | ')
     puts '---+---+---'
@@ -72,6 +74,87 @@ module GameLogic
     puts ' '
 
     p @positions_taken
+  end
+
+  def restart_game_prompt
+    loop do
+      puts 'Do you want to restart the game? (Y/N)'
+      choice = gets.chomp.upcase
+      if choice == 'Y'
+        puts 'Restarting the game...'
+        @p1_positions = []
+        @p2_positions = []
+        @row1 = %w[1 2 3]
+        @row2 = %w[4 5 6]
+        @row3 = %w[7 8 9]
+        @game_over = false
+        @winner = nil
+        @positions_taken = []
+        start
+        break
+      elsif choice == 'N'
+        puts 'Exiting the game. Thank you for playing!'
+        exit
+      else
+        puts 'Invalid choice. Please enter Y or N.'
+      end
+    end
+  end
+
+  def retry_turn_p1
+    puts 'retrying turn...'
+    p1_turn
+  end
+
+  def retry_turn_p2
+    puts 'retrying turn...'
+    p2_turn
+  end
+
+  def p1_turn
+    puts "#{@p1_name}: Enter a position for X".colorize(color: :blue, mode: :bold)
+    position = gets.chomp.to_i
+    puts ' '
+    if @positions_taken.include?(position) || position > 9 || position < 1
+      puts 'Not a valid position'
+      @p1_positions.delete(position)
+      retry_turn_p1
+    else
+      mark_x(position)
+      result = check_for_winner
+      @game_over = true unless result.nil?
+      print_board
+      puts ' '
+    end
+  end
+
+  def p2_turn
+    puts "#{@p2_name}: Enter a position for O".colorize(color: :red, mode: :bold)
+    position = gets.chomp.to_i
+    puts ' '
+    if @positions_taken.include?(position) || position > 9 || position < 1
+      puts 'Not a valid position'
+      @p2_positions.delete(position)
+      retry_turn_p2
+    else
+      mark_o(position)
+      result = check_for_winner
+      @game_over = true unless result.nil?
+      print_board
+      puts ' '
+    end
+  end
+
+  def collect_player_names
+    puts 'Player 1: What is your name?'
+    @p1_name = gets.strip
+    puts "Thanks #{@p1_name}!"
+    puts ' '
+
+    puts 'Player 2: What is your name?'
+    @p2_name = gets.strip
+    puts "Thanks #{@p2_name}!"
+    puts ' '
   end
 
   def check_for_winner
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,95 +1,9 @@
 require_relative 'game_logic'
-require 'pry-byebug'
 require 'colorize'
 require 'colorized_string'
 
 class TicTacToe
   include GameLogic
-
-  @game_over = false
-  @p1_name = 'Player One'
-  @p2_name = 'Player Two'
-
-  def retry_turn_p1
-    puts 'retrying turn...'
-    p1_turn
-  end
-
-  def retry_turn_p2
-    puts 'retrying turn...'
-    p2_turn
-  end
-
-  def p1_turn
-    puts "#{@p1_name}: Enter a position for X".colorize(color: :blue, mode: :bold)
-    position = gets.chomp.to_i
-    puts ' '
-    if @positions_taken.include?(position) || position > 9 || position < 1
-      puts 'Not a valid position'
-      @p1_positions.delete(position)
-      retry_turn_p1
-    else
-      mark_x(position)
-      result = check_for_winner
-      @game_over = true unless result.nil?
-      print_board
-      puts ' '
-    end
-  end
-
-  def p2_turn
-    puts "#{@p2_name}: Enter a position for O".colorize(color: :red, mode: :bold)
-    position = gets.chomp.to_i
-    puts ' '
-    if @positions_taken.include?(position) || position > 9 || position < 1
-      puts 'Not a valid position'
-      @p2_positions.delete(position)
-      retry_turn_p2
-    else
-      mark_o(position)
-      result = check_for_winner
-      @game_over = true unless result.nil?
-      print_board
-      puts ' '
-    end
-  end
-
-  def restart_game_prompt
-    loop do
-      puts 'Do you want to restart the game? (Y/N)'
-      choice = gets.chomp.upcase
-      if choice == 'Y'
-        puts 'Restarting the game...'
-        @p1_positions = []
-        @p2_positions = []
-        @row1 = %w[1 2 3]
-        @row2 = %w[4 5 6]
-        @row3 = %w[7 8 9]
-        @game_over = false
-        @winner = nil
-        @positions_taken = []
-        start
-        break
-      elsif choice == 'N'
-        puts 'Exiting the game. Thank you for playing!'
-        exit
-      else
-        puts 'Invalid choice. Please enter Y or N.'
-      end
-    end
-  end
-
-  def collect_player_names
-    puts 'Player 1: What is your name?'
-    @p1_name = gets.strip
-    puts "Thanks #{@p1_name}!"
-    puts ' '
-
-    puts 'Player 2: What is your name?'
-    @p2_name = gets.strip
-    puts "Thanks #{@p2_name}!"
-    puts ' '
-  end
 
   def start
     puts ' '
